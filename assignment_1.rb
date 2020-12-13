74004f32adf81016d52b01fedee4656ad7fdb899
Raghu-mdsol <rmudhol@mdsol.com>
2020-12-13 23:47:27 +0530

    Assignment1

---
+++ assignment_1.rb
@@ -0,0 +1,98 @@
+$count = 0
+class Customer 
+
+    attr_accessor :id , :name, :points, :group,:avg
+
+   #parameterized constructor
+   def initialize(id,name)
+   @id = id 
+   @name = name 
+   end
+
+    def addPoints(given_amount)
+      @points = given_amount
+
+    end
+
+    def upgradePoints(given_percentage)
+        @upgradePoint = @points * (given_percentage)/10
+        @points = @points + @upgradePoint
+    end
+
+   def removePoints(a)
+    @points  = @points-a
+   end
+
+      def display 
+        $count =  $count + 1
+       
+        puts "…………………………… Customer: #{$count} ID: #{@id}  Name: #{@name} Points: #{@points} Group: null "
+      end
+
+    def self.getBest(get_arr)
+
+     m = get_arr.max
+     puts "MAX: #{m}"
+    end
+    
+    def self.calAvg(get_arr)
+      avg = get_arr.sum/$count
+    end
+ 
+      def self.displayAvg(val)
+          avg = val
+         puts "Average value is #{avg}"
+     end
+
+end
+
+class Test 
+    
+    def display
+    
+    arr = []
+
+    c0 = Customer.new(1543 ,"Nimal")
+    c0.addPoints(100)
+    arr << c0.points
+    c0.display
+
+    c1 = Customer.new(6561,"Saman")
+    c1.addPoints(200)
+    b = c1.upgradePoints(2)
+    arr << c1.points
+    c1.display
+
+    c2 = Customer.new(6954,"Kasun")
+    a = c2.addPoints(3258)
+    a = c2.addPoints(1000)
+    arr << c2.points
+    c2.display
+
+    c3 = Customer.new(3485,"Nayana")
+    c3.addPoints(59)
+    arr << c3.points
+    c3.display
+
+    c4 = Customer.new(8546,"Kalpa")
+    p = c4.addPoints(1652)
+    arr << c4.points
+    c4.display
+
+    a = Customer.calAvg(arr)
+    Customer.displayAvg(a)
+    Customer.getBest(arr) # highest number of points using the method getBest
+
+    cust = c4   #reference variable called cust of type Customer. 
+    cust.display
+    cust.removePoints(a)
+    cust.display
+    cust = c3 #Copy the values of the attributes of the fourth customer 
+    cust1 = cust
+    cust1 = nil
+    # cust1.id 
+    end
+end
+
+ t = Test.new
+ t.display
