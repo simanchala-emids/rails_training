d53e99b052011f71e2d2f103cc44d50bddcf8eb8
Raghu-mdsol <rmudhol@mdsol.com>
2020-12-23 10:49:26 +0530

    removed unnecessary code

--- assignment_1.rb
+++ assignment_1.rb
@@ -2,39 +2,38 @@ $count = 0
 class Customer
   attr_accessor :id, :name, :points, :group, :avg
 
-  def initialize(cust_id, name)
-    @id = cust_id
+  def initialize(id, name)
+    @id = id
     @name = name
+    @points = 0
   end
 
-  def add_points(given_amount)
-    @points = given_amount
+  def add_points(point)
+    @points = @points + point
   end
 
-  def upgrade_points(given_percentage)
-    @upgradepoint = @points * given_percentage / 10
-    @points += @upgradepoint
+  def upgrade_points(percentage)
+    @points = @points * (100 + percentage) / 100
   end
 
-  def remove_points(*)
-    @points -= @remove_point
-    if @points.negative? puts "Amount value is negative so i'm making it to #{@points}"
-    elsif puts "Amount value is not negative so i'm making it to #{@points}"
+  def remove_points(point)
+    @points = @points - point
+    if @points < 0
+      puts "Amount value is negative so i'm making it to point value is zero"
+    else
+      puts "Amount value is not negative so i'm making it to #{@points}"
     end
   end
 
-  def compute_group(points, group)
-    @point = points
-    @group = group
-      @i = 0
-    while @points.positive?
-      if @point < 100 && @points > 1
+  def compute_group(points)
+    while points.positive?
+      if points < 100 && points > 1
         @group = 'Silver'
-      elsif @point <= 500 && @point > 100
+      elsif points <= 500 && points > 100
         @group = 'Gold'
-      elsif @point <= 2000 && @point > 500
+      elsif points <= 2000 && points > 500
         @group = 'Diamond'
-      elsif @point > 2000
+      elsif points > 2000
         @group = 'Platinum'
       end
         break
@@ -43,7 +42,7 @@ class Customer
 
   def display
     $count = $count + 1
-    puts "…………………………… Customer: #{$count} Id: #{@cust_id}  Name: #{@name} Points: #{@points} Group: #{@group} "
+    puts "…………………………… Customer: #{$count} Id: #{@id}  Name: #{@name} Points: #{@points} Group: #{@group} "
   end
 
   def self.get_best(get_arr)
@@ -70,7 +69,7 @@ class Test
     c0 = Customer.new(1543, 'Nimal')
     c0.add_points(129)
     arr << c0.points
-    c0.compute_group(c0.points, 'metal')
+    c0.compute_group(c0.points)
     c0.display
     c << c0
 
@@ -78,42 +77,41 @@ class Test
     c1.add_points(785)
     c1.upgrade_points(2)
     arr << c1.points
-    c1.compute_group(c1.points, 'metal')
+    c1.compute_group(c1.points)
     c1.display
     c << c1
 
     c2 = Customer.new(6954, 'Kasun')
     a = c2.add_points(3258)
-    # a = c2.add_points(1000)
+    a = c2.add_points(1000)
     arr << c2.points
-    c2.compute_group(c2.points, 'metal')
+    c2.compute_group(c2.points)
     c2.display
     c << c2
 
     c3 = Customer.new(3485, 'Nayana')
     c3.add_points(59)
     arr << c3.points
-    c3.compute_group(c3.points, 'metal')
+    c3.compute_group(c3.points)
     c3.display
     c << c3
 
     c4 = Customer.new(8546, 'Kalpa')
     p = c4.add_points(1652)
     arr << c4.points
-    c4.compute_group(c4.points, 'metal')
+    c4.compute_group(c4.points)
     c << c4
     c[4].display
-    c4.compute_group(5000, 'silver_diamond')
+    c4.compute_group(5000)
     c4.display
     a = Customer.cal_avg(arr)
     Customer.display_avg(a)
-    # highest number of points using the method get_best
     Customer.get_best(arr)
 
     cust = Customer.new(1543, 'Nimal')
     cust = c3
     cust.display
-    # cust.remove_points(2000)
+    cust.remove_points(2000)
     cust.display
 
     cust1 = Customer.new(1, 'Raghu')
@@ -123,5 +121,5 @@ class Test
   end
 end
 
- t = Test.new
- t.display
+t = Test.new
+t.display
