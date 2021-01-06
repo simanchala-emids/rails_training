7866ea007cd4137cddae2528bc6d767cb942a1dc
Raghu-mdsol <rmudhol@mdsol.com>
2021-01-06 15:50:45 +0530

    controller and routes examples

--- Demo/config/routes.rb
+++ Demo/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #get 'student/index'
+  #match "student/index", :to => "student#index", :via => :get
+  #get "student/show"
+  #get "student/create"
+  #match "raghav/mudhol", :to => "student#index", :via => :get
+  #get "student/index"
+  #root "student#show"
+  get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
